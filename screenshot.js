3030d59da71311a236429d47a241b76c50ee3dba
mikexql <mclokix@gmail.com>
2025-06-30 13:11:04 +0800

    testing chromium options

--- screenshot.js
+++ screenshot.js
@@ -2,9 +2,17 @@ const { chromium } = require('playwright');
 const path = require('path');
 
 module.exports = async function takeScreenshot(url) {
-    const browser = await chromium.launch({ headless: true });
+    const browser = await chromium.launch({ headless: true,
+        args: [
+            '--no-sandbox',
+            '--disable-dev-shm-usage',
+            '--disable-gpu',
+            '--disable-background-timer-throttling',
+            '--disable-renderer-backgrounding',
+            '--disable-backgrounding-occluded-windows',
+        ],});
     const page = await browser.newPage();
-    await page.goto(url, { waitUntil: 'networkidle' });
+    await page.goto(url, { waitUntil: 'load' });
 
     const filename = `screenshot-${Date.now()}.png`;
     const filepath = path.join('/tmp', filename); // << use /tmp on Render
